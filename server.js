23e20c6758938421cbdb067a7ae76e82b2bc91b8
sh78ba <sh78ba@gmail.com>
2024-09-14 13:45:33 +0530

    backend

---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const app=express()
+const cors=require("cors")
+require('dotenv').config()
+
+
+//fors cors
+app.use(cors());
+//for getting object in JSON format
+app.use(express.json())
+
+
+
+
+
+//for database conection
+mongoose.connect(process.env.MONGO_URI)
+
+const db=mongoose.connection
+
+db.on("error",()=>{
+    console.log("Error while connecting to Database")
+})
+
+db.once("open",()=>{
+    console.log("Successfull connecting the database")
+})
+
+
+
+//stitch 
+require("./routes/patient.routes")(app);
+require("./routes/appointment.route")(app);
+require("./routes/upload.route")(app);
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server started at port",process.env.PORT)
+})
+
+
+
